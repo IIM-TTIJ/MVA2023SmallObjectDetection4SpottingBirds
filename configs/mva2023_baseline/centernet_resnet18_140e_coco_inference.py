43cca723b5ef2df140cefb0824876f7988bc70b9
cyoukaikai <40422292+cyoukaikai@users.noreply.github.com>
2023-01-10 17:41:34 +0900

    update path_error_for_inference_ann_file

--- configs/mva2023_baseline/centernet_resnet18_140e_coco_inference.py
+++ configs/mva2023_baseline/centernet_resnet18_140e_coco_inference.py
@@ -4,7 +4,7 @@ data_root = 'data/'
 data = dict(
     test=dict(
         samples_per_gpu=4,
-        ann_file=data_root + 'mva2023_sod4bird_pub_test/public_test_coco_empty_ann.json',
+        ann_file=data_root + 'mva2023_sod4bird_pub_test/annotations/public_test_coco_empty_ann.json',
         img_prefix=data_root + 'mva2023_sod4bird_pub_test/images/',
     ) 
 )
