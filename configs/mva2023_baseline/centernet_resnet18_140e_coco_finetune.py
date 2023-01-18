f407c78038be6298ada7f0bfbf517ef021b3c1d0
Yuki Kondo <62691917+Yuki-11@users.noreply.github.com>
2023-01-18 17:40:26 +0900

    Fix load_from of centernet_resnet18_140e_coco_finetune.py

--- configs/mva2023_baseline/centernet_resnet18_140e_coco_finetune.py
+++ configs/mva2023_baseline/centernet_resnet18_140e_coco_finetune.py
@@ -27,5 +27,5 @@ lr_config = dict(
     step=[15, 18])
 runner = dict(max_epochs=20)
 evaluation = dict(interval=20, metric='bbox')
-load_from = LIB_ROOT_DIR + 'work_dirs/centernet_resnet18_140e_coco/latest.pth'
+load_from = LIB_ROOT_DIR + '/work_dirs/centernet_resnet18_140e_coco/latest.pth'
 checkpoint_config = dict(interval=1)
